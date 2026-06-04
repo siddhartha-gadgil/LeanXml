ee3c5c87688fa4a2e25d8b3f59110bd907e8d472
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2026-06-04 13:06:04 +0530

    initial commit

---
+++ LeanXml.lean
@@ -0,0 +1,4 @@
+-- This module serves as the root of the `LeanXml` library.
+-- Import modules here that should be built as part of the library.
+import LeanXml.Basic
+import LeanXml.Parser
---
+++ Main.lean
@@ -0,0 +1,6 @@
+import LeanXml
+
+def hello := "world"
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
